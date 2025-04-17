25c9b875435e0871a102d1404410abd3c23c76e9
supriya7768 <mahajansupriya77@gmail.com>
2025-04-17 15:12:53 +0530

    Input property

---
+++ Angular/Demo_Project/src/app/parent/parent.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.sass']
+})
+export class ParentComponent {
+
+  fruits : string[] = ['Apple', 'Banana', 'Grapes', 'Cherry' ];
+
+  isTrue = true;
+
+  userName : string = 'Bob';
+}
